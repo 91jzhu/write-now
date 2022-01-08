d48b87c586cde1e6bf08ebeb908d891975a8f1c8
91jzhu <j1712032377@163.com>
2022-01-08 17:14:36 +0800

    初始化项目，添加侧边栏 Sidebar

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login.vue",
+  data(){
+    return{
+      msg:'this is login'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
